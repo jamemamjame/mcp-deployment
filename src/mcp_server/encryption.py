4a2357f8ae157244771a927f2944d6aacdf21059
jamemamjame <jamemamjame@gmail.com>
2026-01-01 01:39:24 +0700

    fix: specify host and port for MCP server initialization

--- src/mcp_server/encryption.py
+++ src/mcp_server/encryption.py
@@ -2,7 +2,7 @@
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
-mcp = FastMCP("jame-encryption-server")
+mcp = FastMCP("jame-encryption-server", host="0.0.0.0", port=8000)
 
 
 # Add an addition tool
